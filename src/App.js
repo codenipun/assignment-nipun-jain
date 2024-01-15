eba3167227c04042cb366d83869d8d6531df1e7b
Nipun Jain <nipunjain119@gmail.com>
2024-01-16 03:07:29 +0530

    final commit assignment done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+let list = [
+  {
+    id: 101,
+    name: "John",
+    email:"john43@gmail.com"
+  },
+  {
+    id: 102,
+    name: "Aman",
+    email:"aman34@gmail.com"
+  },
+  {
+    id: 103,
+    name: "Sumit",
+    email:"sumit9876@gmail.com"
+  },
+  {
+    id: 104,
+    name: "Vinay",
+    email:"vinay456@gmail.com"
+  },
+  {
+    id: 105,
+    name: "Nipun",
+    email:"nipun68@gmail.com"
+  },
+  {
+    id: 106,
+    name: "Tanya",
+    email:"tanya098@gmail.com"
+  },
+  {
+    id: 107,
+    name: "Sawan",
+    email:"sawan4567@gmail.com"
+  },
+]
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const [dummyData, setDummyData] = useState(list)
+  const [searchInput, setSearchInput] = useState("");
+  const [selectedData, setSelectedData] = useState([]);
+
+  function handleItemClick(id, name, email) {
+    setSelectedData([...selectedData, { id: id, name: name, email:email }]);
+    setDummyData(dummyData.filter((item) => item.id !== id))
+  }
+
+  function handleRemoveItem(id, name, email){
+    setDummyData([...dummyData, {id:id, name:name, email:email}])
+    setSelectedData(selectedData.filter((item)=>item.id!==id));
+  }
+
+  function handleSearch(event){
+    console.log(event.target.value)
+    setSearchInput(event.target.value);
+    if(event.target.value) setDummyData(dummyData.filter((item)=>item.name.toLowerCase().includes(searchInput.toLowerCase())));
+    else setDummyData(list.filter(item => !selectedData.some(selected => selected.id === item.id)));
+  }
+
+  console.log(dummyData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainApp">
+      <div className="App">
+        <div className="selectedItems">
+          {selectedData.map((item) => (
+            <div key={item.id} className="item">
+              <div className="itemLogo">{item.name[0]}</div>
+              <text>{item.name}</text>
+              <text className="crossIcon" onClick={()=>handleRemoveItem(item.id, item.name, item.email)}>X</text>
+            </div>
+          ))}
+        </div>
+      <div className="Container1">
+        <input
+          type="text"
+          className="inputfield"
+          placeholder="Enter some name"
+          onClick={() => !open && setOpen(!open)}
+          onChange={(e)=>handleSearch(e)}
+        />
+      {open && (
+        dummyData.length!==0 && <div className="dummy_data_container">
+          {dummyData.map((item) => (
+            <div key={item.id} className="dummy_item"
+              onClick={() => handleItemClick(item.id, item.name, item.email)}
+            >
+              <div className="itemLogo">{item.name[0]}</div>
+              <text><span className="itemName">{item.name}</span> - <span className="itemEmail">{item.email}</span></text>
+            </div>
+          ))}
+        </div>
+      )}
+      </div>
+    </div>
+      <div className="footer">Designed By <a href="https://linkedin.com/in/nipun18" target="_blank" className="name">Nipun Jain</a></div>
     </div>
   );
 }
